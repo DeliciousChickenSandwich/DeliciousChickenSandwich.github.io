f7b8f8b5b04555efdddc4fedea707b84200e4825
DeliciousChickenSandwich <macneil729@gmail.com>
2017-02-21 20:48:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
   createCanvas(1600,900);
-  background(200,0,200);
+  background(126,0,126);
 }
 var x = 0;
 
 function draw() {
-  stroke(255,0,0);
-  fill(60,60,x);
+  stroke(0,50,0);
+  fill(0,0,x);
   rect(mouseX,mouseY,3,3);
-  x = x + 3;
+  x = x + 10;
   if(x > 255) {
     x = x - 255;
   }
