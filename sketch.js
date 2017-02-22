9ced317d1503600b11b30b115c1b1ad94e3fed33
DeliciousChickenSandwich <macneil729@gmail.com>
2017-02-21 20:32:32 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,9 +6,12 @@ var x = 0;
 
 function draw() {
   stroke(0,255,0);
-  fill(0,0,x);
+  fill(60,60,x);
   ellipse(mouseX,mouseY,700,50);
   var offset = random(1);
   x = x + 3;
+  if(x > 255) {
+    x = x - 255;
+  }
 
 }
