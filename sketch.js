632d8341928fb6e109e3f2d5d670f3af989be5b8
DeliciousChickenSandwich <macneil729@gmail.com>
2017-02-21 20:16:57 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,9 +2,14 @@ function setup() {
   createCanvas(1600,900);
   background(0,0,255);
 }
+var x = 0; var y = 120; var z = 260;
 
 function draw() {
   stroke(0,255,0);
-  fill(255,0,0);
-  rect(mouseX-50,mouseY-50,100,100);
+  fill(x,y,z);
+  ellipse(mouseX-50,mouseY-50,100,100);
+  var offset = random(1);
+  x = (x + random(5)) * offset;
+  y = (y + random(5)) * offset;
+  z = (z + random(5)) * offset;
 }
