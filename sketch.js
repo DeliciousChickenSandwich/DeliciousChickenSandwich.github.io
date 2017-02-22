79fe3791bb30eea2a18d38a15440382a9f39ce20
DeliciousChickenSandwich <macneil729@gmail.com>
2017-02-21 20:09:52 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,8 @@
-
 function setup() {
-  //make a screen for painting
-  createCanvas(1000,900);
-
-    background(100,250,255);
+  createCanvas(1600,900);
+  background(0,0,255);
 }
 
-
-function draw()
- {
-  //make it cyan
-
-  stroke(0,0,255);
- 
- fill(20,255,255);
- 
- // make a rectangle in the middle of the mouse
- 
- rect(mouseX - 40,mouseY - 40,80,80);
-}
-
-
-function mousePressed() 
-{
-    background(random(255),random(255),random(255));
+function draw() {
+  rect(mouseX-50,mouseY-50,100,100);
 }
